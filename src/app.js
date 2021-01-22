28193e2cab5718ec68bb161b5369b9e7f74e73ae
marssaljr <marcaljunior8@gmail.com>
2021-01-22 03:15:43 +0000

    changing route

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const router = express.Router();
 const cors = require('cors')
-const mail = require('./email')
+const mail = require('./email.js');
 require('dotenv').config()
 
 app.use(cors())
-app.use(express.static("../front"))
+app.use(express.static("src/front"));
 
 app.get("/", (req, res) => {
-	res.sendFile("../index.html")
+	res.sendFile("/front/")
 })
 app.post("/sendmail/:encoded?", (req, res) => {
 	res.send(mail.verify(req.params.encoded))
 })
-
+app.use('/', router);
 app.listen(process.env.PORT, console.log("Server started!"))
